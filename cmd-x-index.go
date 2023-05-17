aa673e5e84d2233fe977703cc5fff37c33ad85f6
gagliardetto <gagliardetto@users.noreply.github.com>
2023-05-17 10:18:16 +0200

    Add CID to offset indexer command

---
+++ cmd-x-index.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/urfave/cli/v2"
+	"k8s.io/klog/v2"
+)
+
+func newXIndexCmd() *cli.Command {
+	var verify bool
+	return &cli.Command{
+		Name:        "x-index",
+		Description: "Given a CAR file containing a Solana epoch, create an index of the file that maps CIDs to offsets in the CAR file.",
+		ArgsUsage:   "<car-path> <index-dir>",
+		Before: func(c *cli.Context) error {
+			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "verify",
+				Usage:       "verify the index after creating it",
+				Destination: &verify,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			carPath := c.Args().Get(0)
+			indexDir := c.Args().Get(1)
+
+			{
+				startedAt := time.Now()
+				defer func() {
+					klog.Infof("Finished in %s", time.Since(startedAt))
+				}()
+				klog.Infof("Creating index for %s", carPath)
+				indexFilepath, err := CreateCompactIndex_CIDToOffset(
+					context.TODO(),
+					carPath,
+					indexDir,
+				)
+				if err != nil {
+					panic(err)
+				}
+				klog.Info("Index created")
+				if verify {
+					klog.Infof("Verifying index for %s located at %s", carPath, indexFilepath)
+					startedAt := time.Now()
+					defer func() {
+						klog.Infof("Finished in %s", time.Since(startedAt))
+					}()
+					err := VerifyIndex(context.TODO(), carPath, indexFilepath)
+					if err != nil {
+						return cli.Exit(err, 1)
+					}
+					klog.Info("Index verified")
+					return nil
+				}
+			}
+			return nil
+		},
+	}
+}
