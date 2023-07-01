c40fbc2b57c172a59360d6c126600f130c24894d
gagliardetto <gagliardetto@users.noreply.github.com>
2023-07-01 18:13:06 +0200

    Filecoin rpc server: allow loading config from file instead of args

--- cmd-rpc-server-filecoin.go
+++ cmd-rpc-server-filecoin.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/rpcpool/yellowstone-faithful/compactindex36"
 	"github.com/rpcpool/yellowstone-faithful/gsfa"
 	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v3"
 )
 
 func newCmd_rpcServerFilecoin() *cli.Command {
@@ -24,37 +28,45 @@ func newCmd_rpcServerFilecoin() *cli.Command {
 				Value:       ":8899",
 				Destination: &listenOn,
 			},
+			&cli.StringFlag{
+				Name:  "config",
+				Usage: "Global config filepath",
+				Value: "",
+			},
 		},
 		Action: func(c *cli.Context) error {
-			slotToCidIndexFilepath := c.Args().Get(0)
-			if slotToCidIndexFilepath == "" {
+			config, err := rpcServerFilecoinLoadConfig(c)
+			if err != nil {
+				return fmt.Errorf("failed to load config: %w", err)
+			}
+			spew.Dump(config)
+			if config.Indexes.SlotToCid == "" {
 				return cli.Exit("Must provide a slot-to-CID index filepath/url", 1)
 			}
-			sigToCidIndexFilepath := c.Args().Get(1)
-			if sigToCidIndexFilepath == "" {
+			if config.Indexes.SigToCid == "" {
 				return cli.Exit("Must provide a signature-to-CID index filepath/url", 1)
 			}
 
-			slotToCidIndexFile, err := openIndexStorage(slotToCidIndexFilepath)
+			slotToCidIndexFile, err := openIndexStorage(config.Indexes.SlotToCid)
 			if err != nil {
-				return fmt.Errorf("failed to open index file: %w", err)
+				return fmt.Errorf("failed to open slot-to-cid index file: %w", err)
 			}
 			defer slotToCidIndexFile.Close()
 
 			slotToCidIndex, err := compactindex36.Open(slotToCidIndexFile)
 			if err != nil {
-				return fmt.Errorf("failed to open index: %w", err)
+				return fmt.Errorf("failed to open slot-to-cid index: %w", err)
 			}
 
-			sigToCidIndexFile, err := openIndexStorage(sigToCidIndexFilepath)
+			sigToCidIndexFile, err := openIndexStorage(config.Indexes.SigToCid)
 			if err != nil {
-				return fmt.Errorf("failed to open index file: %w", err)
+				return fmt.Errorf("failed to open sig-to-cid index file: %w", err)
 			}
 			defer sigToCidIndexFile.Close()
 
 			sigToCidIndex, err := compactindex36.Open(sigToCidIndexFile)
 			if err != nil {
-				return fmt.Errorf("failed to open index: %w", err)
+				return fmt.Errorf("failed to open sig-to-cid index: %w", err)
 			}
 
 			ls, err := newLassieWrapper(c)
@@ -63,9 +75,8 @@ func newCmd_rpcServerFilecoin() *cli.Command {
 			}
 
 			var gsfaIndex *gsfa.GsfaReader
-			gsfaIndexDir := c.Args().Get(4)
-			if gsfaIndexDir != "" {
-				gsfaIndex, err = gsfa.NewGsfaReader(gsfaIndexDir)
+			if config.Indexes.Gsfa != "" {
+				gsfaIndex, err = gsfa.NewGsfaReader(config.Indexes.Gsfa)
 				if err != nil {
 					return fmt.Errorf("failed to open gsfa index: %w", err)
 				}
@@ -82,4 +93,92 @@ func newCmd_rpcServerFilecoin() *cli.Command {
 			)
 		},
 	}
+}
+
+func rpcServerFilecoinLoadConfig(c *cli.Context) (*RpcServerFilecoinConfig, error) {
+	// Either load from config file or from args:
+	cfg := &RpcServerFilecoinConfig{}
+	if slotToCidIndexFilepath := c.Args().Get(0); slotToCidIndexFilepath != "" {
+		cfg.Indexes.SlotToCid = slotToCidIndexFilepath
+	}
+	if sigToCidIndexFilepath := c.Args().Get(1); sigToCidIndexFilepath != "" {
+		cfg.Indexes.SigToCid = sigToCidIndexFilepath
+	}
+	if gsfaIndexDir := c.Args().Get(2); gsfaIndexDir != "" {
+		cfg.Indexes.Gsfa = gsfaIndexDir
+	}
+
+	// if a global config file is specified, load it:
+	if configFilepath := c.String("config"); configFilepath != "" {
+		globalConfig, err := loadGlobalConfig(configFilepath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load global config: %w", err)
+		}
+		if cfg.Indexes.SlotToCid == "" {
+			cfg.Indexes.SlotToCid = globalConfig.Indexes.SlotToCid
+		}
+		if cfg.Indexes.SigToCid == "" {
+			cfg.Indexes.SigToCid = globalConfig.Indexes.SigToCid
+		}
+		if cfg.Indexes.Gsfa == "" {
+			cfg.Indexes.Gsfa = globalConfig.Indexes.Gsfa
+		}
+	}
+
+	return cfg, nil
+}
+
+func loadGlobalConfig(configFilepath string) (*RpcServerFilecoinConfig, error) {
+	cfg := &RpcServerFilecoinConfig{}
+	err := cfg.load(configFilepath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load global config: %w", err)
+	}
+	return cfg, nil
+}
+
+func (cfg *RpcServerFilecoinConfig) load(configFilepath string) error {
+	// if is json, load from json:
+	if isJSONFile(configFilepath) {
+		return cfg.loadFromJSON(configFilepath)
+	}
+	if isYAMLFile(configFilepath) {
+		return cfg.loadFromYAML(configFilepath)
+	}
+	return fmt.Errorf("unknown file type for global config: %s", configFilepath)
+}
+
+func (cfg *RpcServerFilecoinConfig) loadFromJSON(configFilepath string) error {
+	file, err := os.Open(configFilepath)
+	if err != nil {
+		return fmt.Errorf("failed to open global config file: %w", err)
+	}
+	defer file.Close()
+	return json.NewDecoder(file).Decode(cfg)
+}
+
+func (cfg *RpcServerFilecoinConfig) loadFromYAML(configFilepath string) error {
+	file, err := os.Open(configFilepath)
+	if err != nil {
+		return fmt.Errorf("failed to open global config file: %w", err)
+	}
+	defer file.Close()
+
+	return yaml.NewDecoder(file).Decode(cfg)
+}
+
+func isJSONFile(filepath string) bool {
+	return filepath[len(filepath)-5:] == ".json"
+}
+
+func isYAMLFile(filepath string) bool {
+	return filepath[len(filepath)-5:] == ".yaml" || filepath[len(filepath)-4:] == ".yml"
+}
+
+type RpcServerFilecoinConfig struct {
+	Indexes struct {
+		SlotToCid string `json:"slot_to_cid" yaml:"slot_to_cid"`
+		SigToCid  string `json:"sig_to_cid" yaml:"sig_to_cid"`
+		Gsfa      string `json:"gsfa" yaml:"gsfa"`
+	} `json:"indexes" yaml:"indexes"`
 }
