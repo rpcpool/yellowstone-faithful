63cf560af660c8d1175c6d34ec7023c9adf43f1a
gagliardetto <gagliardetto@users.noreply.github.com>
2023-08-19 16:42:15 +0200

    Improve live reload

--- multiepoch.go
+++ multiepoch.go
@@ -75,16 +75,16 @@ func (m *MultiEpoch) RemoveEpoch(epoch uint64) error {
 	return nil
 }
 
-func (m *MultiEpoch) RemoveEpochByConfigFilepath(configFilepath string) error {
+func (m *MultiEpoch) RemoveEpochByConfigFilepath(configFilepath string) (uint64, error) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	for epoch, ep := range m.epochs {
 		if ep.config.ConfigFilepath() == configFilepath {
 			delete(m.epochs, epoch)
-			return nil
+			return epoch, nil
 		}
 	}
-	return fmt.Errorf("epoch not found for config file %q", configFilepath)
+	return 0, fmt.Errorf("epoch not found for config file %q", configFilepath)
 }
 
 func (m *MultiEpoch) ReplaceEpoch(epoch uint64, ep *Epoch) error {
