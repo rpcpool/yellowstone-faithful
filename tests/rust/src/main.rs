9db7ac0b5638ecda0ac9a70c885d9aaf14d4cf04
linuskendall <git@linuskendall.com>
2023-11-05 22:36:12 +0000

    Updated rust tests

--- tests/rust/src/main.rs
+++ tests/rust/src/main.rs
@@ -1,6 +1,9 @@
-use solana_sdk::commitment_config::CommitmentConfig;
-use solana_client::{ rpc_client::RpcClient, rpc_config::RpcBlockConfig };
+use solana_sdk::{commitment_config::CommitmentConfig, signature::Signature};
+use solana_client::{ rpc_client::RpcClient, rpc_config::RpcBlockConfig, rpc_config::RpcTransactionConfig };
 use solana_transaction_status::{ TransactionDetails, UiTransactionEncoding };
+
+use std::str::FromStr;
+
 
 fn main() {
     let faithful_rpc = RpcClient::new("https://rpc.old-faithful.net".to_string());
@@ -8,11 +11,11 @@ fn main() {
     let block = faithful_rpc.get_block_with_config(
         209520021,
         RpcBlockConfig {
-            transaction_details: Some(TransactionDetails::Full),
+            transaction_details: Some(TransactionDetails::None),
             commitment: Some(CommitmentConfig::finalized()),
             max_supported_transaction_version: Some(0),
             encoding: Some(UiTransactionEncoding::Base64),
-            rewards: Some(true),
+            rewards: Some(false),
         },
     );
     println!("got reply from faithful");
@@ -24,4 +27,24 @@ fn main() {
             println!("Err: {}", e);
         },
     };
+
+    let sig_s = "3qEUUW9fKaZpECvJ87QfZMyVMQjR1GBKnuCDqJMCgxw1sCzrWSU6q5ydEiX1JEJPbQDGaNoxULxmCW6f4mAnNRo2";
+    let sig = Signature::from_str(&sig_s).unwrap();
+    let transaction = faithful_rpc.get_transaction_with_config(
+      &sig,
+      RpcTransactionConfig {
+        encoding: Some(UiTransactionEncoding::Base64),
+        commitment: Some(CommitmentConfig::finalized()),
+        max_supported_transaction_version: Some(0),
+      },
+      );
+  match transaction {
+      Ok(transaction) => {
+        println!("{:?}", transaction);
+      },
+      Err(e) => {
+        println!("Err: {:?}", e);
+      },
+    };
+      
 }
