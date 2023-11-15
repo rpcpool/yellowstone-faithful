4d4213f403821389dd12f4885e749bed395ac773
gagliardetto <gagliardetto@users.noreply.github.com>
2023-11-15 15:59:42 +0100

    Include genesis reading/parsing code from https://github.com/firedancer-io/radiance

---
+++ radiance/genesis/genesis.go
@@ -0,0 +1,38 @@
+package genesis
+
+import (
+	"time"
+
+	"github.com/rpcpool/yellowstone-faithful/radiance/runtime"
+)
+
+// Genesis contains the genesis state of a Solana ledger.
+type Genesis struct {
+	CreationTime  time.Time
+	Accounts      []AccountEntry
+	Builtins      []BuiltinProgram
+	RewardPools   []AccountEntry
+	TicksPerSlot  uint64
+	PohParams     runtime.PohParams
+	Fees          runtime.FeeParams
+	Rent          runtime.RentParams
+	Inflation     runtime.InflationParams
+	EpochSchedule runtime.EpochSchedule
+	ClusterID     uint32
+}
+
+type AccountEntry struct {
+	Pubkey [32]byte
+	runtime.Account
+}
+
+type BuiltinProgram struct {
+	Key    string
+	Pubkey [32]byte
+}
+
+func (g *Genesis) FillAccounts(state runtime.Accounts) {
+	for _, acc := range g.Accounts {
+		state.SetAccount(&acc.Pubkey, &acc.Account)
+	}
+}
