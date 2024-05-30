d9379a35e76a2d45f7d4b65c4e575bd7800f9bc6
Kirill Fomichev <fanatid@ya.ru>
2024-05-30 13:53:14 -0400

    adjust ci

--- ci/rust-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Prints Rust version
-
-while read -r name equals value _; do
-  if [[ $name = "channel" && $equals = = ]]; then
-    echo "${value//\"/}"
-  fi
-done < <(cat $(dirname "${BASH_SOURCE[0]}")/../rust-toolchain.toml)
