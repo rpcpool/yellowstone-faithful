11a4581779cf041eebc5b6b91647ff098bd21bf7
Kirill Fomichev <fanatid@ya.ru>
2024-06-19 23:31:49 -0400

    add block decode

--- old-faithful-proto/src/lib.rs
+++ old-faithful-proto/src/lib.rs
@@ -6,19 +6,51 @@ pub mod proto {
 
 pub mod decode {
     use {
-        super::{proto, solana::StoredConfirmedBlockTransactionStatusMeta},
+        super::{
+            proto,
+            solana::{StoredConfirmedBlockRewards, StoredConfirmedBlockTransactionStatusMeta},
+        },
         anyhow::Context,
         prost_011::Message,
-        solana_sdk::transaction::VersionedTransaction,
+        solana_sdk::{
+            hash::{Hash, HASH_BYTES},
+            transaction::VersionedTransaction,
+        },
         solana_storage_proto::convert::generated,
         solana_transaction_status::{
-            ConfirmedBlock, ConfirmedTransactionWithStatusMeta, TransactionStatusMeta,
+            ConfirmedBlock, ConfirmedTransactionWithStatusMeta, Reward, TransactionStatusMeta,
             TransactionWithStatusMeta, VersionedTransactionWithStatusMeta,
         },
     };
 
     pub fn confirmed_block(block: proto::BlockResponse) -> anyhow::Result<ConfirmedBlock> {
-        todo!()
+        let previous_blockhash = <[u8; HASH_BYTES]>::try_from(block.previous_blockhash)
+            .map_err(|_error| anyhow::anyhow!("failed to decode previous_blockhash"))?;
+        let blockhash = <[u8; HASH_BYTES]>::try_from(block.blockhash)
+            .map_err(|_error| anyhow::anyhow!("failed to decode blockhash"))?;
+
+        let rewards: Vec<Reward> = match generated::Rewards::decode(block.rewards.as_ref()) {
+            Ok(rewards) => rewards.into(),
+            Err(_error) => bincode::deserialize::<StoredConfirmedBlockRewards>(&block.rewards)
+                .context("failed to decode bincode of Vec<Reward>")?
+                .into_iter()
+                .map(Into::into)
+                .collect(),
+        };
+
+        Ok(ConfirmedBlock {
+            previous_blockhash: Hash::new_from_array(previous_blockhash).to_string(),
+            blockhash: Hash::new_from_array(blockhash).to_string(),
+            parent_slot: block.parent_slot,
+            transactions: block
+                .transactions
+                .iter()
+                .map(versioned_transaction)
+                .collect::<Result<Vec<_>, _>>()?,
+            rewards,
+            block_time: Some(block.block_time),
+            block_height: Some(block.block_height),
+        })
     }
 
     pub fn confirmed_transaction(
@@ -64,7 +96,7 @@ mod solana {
     use {
         serde::{Deserialize, Serialize},
         solana_sdk::{message::v0::LoadedAddresses, transaction::TransactionError},
-        solana_transaction_status::TransactionStatusMeta,
+        solana_transaction_status::{Reward, TransactionStatusMeta},
     };
 
     #[derive(Serialize, Deserialize)]
@@ -100,6 +132,27 @@ mod solana {
                 loaded_addresses: LoadedAddresses::default(),
                 return_data: None,
                 compute_units_consumed: None,
+            }
+        }
+    }
+
+    pub type StoredConfirmedBlockRewards = Vec<StoredConfirmedBlockReward>;
+
+    #[derive(Serialize, Deserialize)]
+    pub struct StoredConfirmedBlockReward {
+        pubkey: String,
+        lamports: i64,
+    }
+
+    impl From<StoredConfirmedBlockReward> for Reward {
+        fn from(value: StoredConfirmedBlockReward) -> Self {
+            let StoredConfirmedBlockReward { pubkey, lamports } = value;
+            Self {
+                pubkey,
+                lamports,
+                post_balance: 0,
+                reward_type: None,
+                commission: None,
             }
         }
     }
