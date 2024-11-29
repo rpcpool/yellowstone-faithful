02ff5570d14b7295f923931020230097dc152161
Pedro <pedrogomesyoo@gmail.com>
2024-11-29 12:03:55 +0700

    fix

--- .github/faithful-data-report.sh
+++ .github/faithful-data-report.sh
@@ -36,6 +36,60 @@ get_txmeta() {
     fi
 }
 
+get_indices() {
+    local epoch=$1
+    local cid_url="$host/$epoch/epoch-$epoch.cid"
+    
+    # First get the CID (BAFY...)
+    if ! check_file_exists "$cid_url"; then
+        echo "n/a"
+        return
+    fi
+    
+    local bafy=$(curl -s "$cid_url")
+    if [[ -z "$bafy" ]]; then
+        echo "n/a"
+        return
+    fi
+
+    # Check all required index files exist
+    local index_files=(
+        "epoch-$epoch-$bafy-mainnet-cid-to-offset-and-size.index"
+        "epoch-$epoch-$bafy-mainnet-sig-to-cid.index"
+        "epoch-$epoch-$bafy-mainnet-sig-exists.index"
+        "epoch-$epoch-$bafy-mainnet-slot-to-cid.index"
+        "epoch-$epoch-gsfa.index.tar.zstd"
+    )
+
+    for file in "${index_files[@]}"; do
+        if ! check_file_exists "$host/$epoch/$file"; then
+            echo "n/a"
+            return
+        fi
+    done
+
+    # If we get here, all files exist
+    echo "$host/$epoch/epoch-$epoch-indices"
+}
+
+get_deals() {
+    local epoch=$1
+    local deals_url="$deals_host/$epoch/deals.csv"
+    if check_file_exists "$deals_url"; then
+        local deals=$(curl -s "$deals_url")
+        # right now it's just checking the deals.csv exists and is longer than 1 line
+        # i.e. we sent a deal that was accepted by an SP
+        # TODO: use `faithful check-deals` to determine if the full epoch can be loaded from filecoin 
+        if [[ -n "$deals" ]] && [[ $(echo "$deals" | wc -l) -gt 1 ]]; then
+            echo "$deals_url"
+        else
+            echo "n/a"
+        fi
+    else
+        echo "n/a"
+    fi
+}
+
 get_size() {
     local epoch=$1
     local size_url="$host/$epoch/epoch-$epoch.car"
@@ -66,7 +120,7 @@ check_file_exists() {
 
 print_empty_row() {
     local epoch=$1
-    echo "| $epoch | n/a | n/a | n/a | n/a | n/a | n/a | n/a | n/a |"
+    echo "| $epoch | n/a | n/a | n/a | n/a  | n/a | n/a | n/a |"
 }
 
 print_row() {
@@ -77,21 +131,27 @@ print_row() {
     local size=$5
     local poh=$6
     local txmeta=$7
+    local deals=$8
+    local indices=$9
 
     # Only create links if the values aren't "n/a"
-    local car_cell="n/a"
-    local sha_cell="n/a"
-    local size_cell="n/a"
-    local poh_cell="n/a"
-    local txmeta_cell="n/a"
+    local car_cell="✗"
+    local sha_cell="✗" 
+    local size_cell="✗"
+    local poh_cell="✗"
+    local txmeta_cell="✗"
+    local deals_cell="✗"
+    local indices_cell="✗"
 
     [[ "$car" != "n/a" ]] && car_cell="[epoch-$epoch.car]($car)"
-    [[ "$sha" != "n/a" ]] && sha_cell="[$sha]($sha_url)"
-    [[ "$size" != "n/a" ]] && size_cell="[$size]($car)"
+    [[ "$sha" != "n/a" ]] && sha_cell="[${sha:0:5}]($sha_url)"
+    [[ "$size" != "n/a" ]] && size_cell="$size GB"
     [[ "$poh" != "n/a" ]] && poh_cell="$poh"
     [[ "$txmeta" != "n/a" ]] && txmeta_cell="$txmeta"
+    [[ "$indices" != "n/a" ]] && indices_cell="✓"
+    [[ "$deals" != "n/a" ]] && deals_cell="[✓]($deals)"
 
-    echo "| $epoch | $car_cell | $sha_cell | $size_cell |  | ✓ | $(date '+%Y-%m-%d %H:%M:%S') | ✓ | ✓ |"
+    echo "| $epoch | $car_cell | $sha_cell | $size_cell | $txmeta_cell | $poh_cell | $indices_cell | $deals_cell |"
 }
 
 CURRENT_EPOCH=$(curl -s https://api.mainnet-beta.solana.com -s -X POST -H "Content-Type: application/json" -d '
@@ -107,9 +167,10 @@ EPOCH_LIST=$(seq 687 -1 670)
 
 # base hostname
 host="https://files.old-faithful.net"
+deals_host="https://filecoin-car-storage-cdn.b-cdn.net/"
 
-echo "| Epoch #  | CAR  | CAR SHA256  | CAR filesize GB | tx meta check | poh check | CAR data created  | Indexes | Filecoin Deals |"
-echo "|---|---|---|---|---|---|---|---|---|"
+echo "| Epoch #  | CAR  | CAR SHA256  | CAR filesize | tx meta check | poh check | Indexes | Filecoin Deals |"
+echo "|---|---|---|---|---|---|---|---|"
 
 for EPOCH in $EPOCH_LIST; do
     CAR=$(get_car_url "$EPOCH")
@@ -120,7 +181,12 @@ for EPOCH in $EPOCH_LIST; do
                  "$CAR" \
                  "$(get_sha "$EPOCH")" \
                  "$SHA_URL" \
-                 "$(get_size "$EPOCH")"
+                 "$(get_size "$EPOCH")" \
+                 "$(get_poh "$EPOCH")" \
+                 "$(get_txmeta "$EPOCH")" \
+                 "$(get_deals "$EPOCH")" \
+                 "$(get_indices "$EPOCH")" \
+
     else
         print_empty_row "$EPOCH"
     fi
