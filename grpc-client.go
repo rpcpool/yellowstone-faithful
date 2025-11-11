81bd551e521dd804f38b665b0789b5bac7926544
gagliardetto <gagliardetto@users.noreply.github.com>
2025-11-11 20:18:05 +0100

    Update grpc-client.go
    Co-authored-by: Miles 'wedtm' Smith <miles@trustless.engineering>

--- grpc-client.go
+++ grpc-client.go
@@ -189,7 +189,7 @@ func newFaithfulClient(serverAddr string, useTLS bool) (*faithfulClient, error) 
 
 	// Add client-side keepalive parameters
 	keepaliveParams := keepalive.ClientParameters{
-		Time:                30 * time.Second, // Ping server if idle for 30 seconds
+		Time:                60 * time.Second, // Ping server if idle for 30 seconds
 		Timeout:             10 * time.Second, // Wait 10 seconds for ping ack
 		PermitWithoutStream: true,             // Allow pings even without active streams
 	}
