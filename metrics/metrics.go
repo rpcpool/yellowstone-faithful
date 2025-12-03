ee494232432227001329760ff3fd5d2608248db9
gagliardetto <gagliardetto@users.noreply.github.com>
2025-12-03 20:39:43 +0100

    add 0.025 to latency buckets

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -140,7 +140,8 @@ var IndexLookupTotal = promauto.NewCounterVec(
 
 var latencyBuckets = []float64{
 	// fractional seconds from 0 to 1, with increments of 0.05 (= 50 ms)
-	0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
+	0, 0.025,
+	0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
 	0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95,
 	1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 10,
 }
