67a718d9f22b52485c2ecbec829d22ae42690dd4
Pedro <pedrogomesyoo@gmail.com>
2026-01-20 14:26:47 +0700

    less buckets

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -139,8 +139,8 @@ var IndexLookupTotal = promauto.NewCounterVec(
 )
 
 var latencyBuckets = []float64{
-	0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 
-	0.6, 0.7, 0.8, 0.9, 1, 
+	0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 
+	0.75, 1, 
 	2.5, 5,
 }
 
